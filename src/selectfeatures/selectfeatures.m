a767492956c6091be50c8b555854daf5f6d73a53
Niccolò Scatena <speedjack95@gmail.com>
2022-10-25 20:08:36 +0200

    Add selectfeatures stage

---
+++ src/selectfeatures/selectfeatures.m
@@ -0,0 +1,24 @@
+function featureList = selectfeatures
+	featureList = {
+		@min;
+		@max;
+		@range;
+		@kurtosis;
+		@skewness;
+		@mean;
+		@median;
+		@mode;
+		@std;
+		@var;
+		@iqr;
+		@mad;
+		@harmmean;
+		@meanfreq;
+		@medfreq;
+		@rms;
+		@peak2rms;
+		@peak2peak;
+		@trapz;
+		"flo"; "fhi"; "bw"; "power"
+	}';
+end
